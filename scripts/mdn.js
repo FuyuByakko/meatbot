48af69307a21fad4ee10cc44c2e4200f2fda886a
Vadim Burlakin <vburlakin@moneytree.jp>
2020-09-03 23:40:47 +0900

    Implement checkPresence action

--- scripts/mdn.js
+++ scripts/mdn.js
@@ -2,8 +2,10 @@
 module.exports = {
   actionDelay: 2000,
   actions: [
-    { type: 'goto', destination: 'https://developer.mozilla.org/en-US/'},
+    { id: 'start', type: 'goto', destination: 'https://developer.mozilla.org/en-US/'},
     { type: 'input', targetSelector: '.search-input-field', text: `String split${String.fromCharCode(13)}`},
     { type: 'click', targetSelector: '#react-container > div.search-results > div:nth-child(2) > div > div:nth-child(1) > a', waitForNavigation: true},
+    { type: 'checkPresence', targetSelector: '.bc-browser-edge', onCheckFail: 'jump', stepId: 'finish', invert: true },
+    { id: 'finish', type: 'goto', destination: 'https://google.com/'}
   ]
 };
