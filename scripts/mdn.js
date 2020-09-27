0977d89d7eb3fa864883e41ff86e534559ce28c4
FuyuByakko <igor.m.byak@gmail.com>
2020-09-27 14:17:00 +0900

    updated base example scripts

--- scripts/mdn.js
+++ scripts/mdn.js
@@ -2,9 +2,8 @@
 module.exports = {
   actionDelay: 500,
   actions: [
-    { id: 'start', type: 'goto', destination: 'https://developer.mozilla.org/en-US/'},
-    { type: 'input', targetSelector: '.search-input-field', text: `String split${String.fromCharCode(13)}`},
-    { type: 'delay', delayTimer: 2000},
+    { id: 'start', type: 'goto', destination: 'https://developer.mozilla.org/en-US/' },
+    { type: 'input', targetSelector: '.search-input-field', text: `String split`, specialKey: "Enter", waitForNavigation: true },
     { type: 'click', targetSelector: '#react-container > div.search-results > div:nth-child(2) > div > div:nth-child(1) > a', waitForNavigation: true},
     { type: 'get', xpath: '//span[@class="seoSummary"]', keyName: 'string.split summary', description: "Save the string.split summary."},
     { type: 'checkPresence', targetSelector: '.bc-browser-edge', onCheckFail: 'jump', stepId: 'finish', invert: true },
